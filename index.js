c3ebf36614115a7f83d688a6debfbc70541d3839
onlynavv <onlynavv@gmail.com>
2021-12-15 14:49:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express, { response } from "express"
+import * as fs from "fs"
+
+const app = express()
+
+const PORT = 9000
+
+const timestamp = Date.now()
+const dataInfo = new Date(timestamp)
+const date = dataInfo.getDate()
+const month = dataInfo.getMonth()
+const year = dataInfo.getFullYear()
+const hours = dataInfo.getHours()
+const minutes = dataInfo.getMinutes()
+const seconds = dataInfo.getSeconds()
+const dateTime = date + "-" + month + "-" + year + "-" + hours + "-" + minutes + "-" + seconds
+
+
+app.get("/", (request, response)=>{
+    response.send("hello node")
+})
+
+app.get("/createFile", (request, response)=>{
+    fs.writeFile(`./newfolder/${dateTime}.txt`, timestamp.toString(), (err)=>{
+        response.send("completed writing")
+    })
+})
+
+app.get("/readFiles", (request, response)=>{
+    fs.readdir("./newfolder", (err, data)=>{
+        response.send(data)
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log("app connected at ", PORT)
+})
